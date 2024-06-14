ddfeff8098c4c61d96194ed71443b6e0574c20e2
inutamago_dogegg <inutamago.dogegg@gmail.com>
2024-06-15 03:06:44 +0900

    便利クラスの追加

---
+++ Assets/h24s_15/Scripts/Utils/Editor/DynamicFontCleaner.cs
@@ -0,0 +1,24 @@
+﻿using TMPro;
+using UnityEditor;
+using UnityEngine;
+
+namespace h24s_15.Utils.Editor {
+    [InitializeOnLoad]
+    public static class DynamicFontCleaner {
+        static DynamicFontCleaner() {
+            EditorApplication.playModeStateChanged += OnPlayModeStateChanged;
+        }
+
+        private static void OnPlayModeStateChanged(PlayModeStateChange state) {
+            if (state == PlayModeStateChange.ExitingPlayMode) {
+                var tmpFontAssets = Resources.FindObjectsOfTypeAll<TMPro.TMP_FontAsset>();
+                foreach (var tmpFontAsset in tmpFontAssets) {
+                    if (tmpFontAsset != null && tmpFontAsset.atlasPopulationMode == AtlasPopulationMode.Dynamic) {
+                        tmpFontAsset.ClearFontAssetData();
+                        Debug.Log("DynamicFontCleaner: ClearFontAssetData " + tmpFontAsset.name);
+                    }
+                }
+            }
+        }
+    }
+}
