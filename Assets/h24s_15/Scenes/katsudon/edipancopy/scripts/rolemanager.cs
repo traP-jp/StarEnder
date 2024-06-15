59849f2b632b902703185cdedc660ff40a1c4852
katsudontech <sumiya.k.ab@m.titech.ac.jp>
2024-06-15 22:26:59 +0900

    ロール開始ボタンを押せるようにしたのと、役牡丹改善

---
+++ Assets/h24s_15/Scenes/katsudon/edipancopy/scripts/rolemanager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rolemanager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnClick()
+    {
+        Rolestart();
+    }
+    void Rolestart()
+    {
+        Debug.Log("rjeilrje");
+    }
+}
--- Assets/h24s_15/Scenes/katsudon/edipancopy/scripts/yakuviewmanage.cs
+++ Assets/h24s_15/Scenes/katsudon/edipancopy/scripts/yakuviewmanage.cs
@@ -3,15 +3,16 @@ using System.Collections.Generic;
 using System.Data.Common;
 using UnityEngine;
 using DG.Tweening;
+using Unity.VisualScripting;
 
 public class NewBehaviourScript : MonoBehaviour
 {
     private bool btnF;
-
+    public GameObject dark;
     // Start is called before the first frame update
     void Start()
     {
-        
+        dark.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -22,15 +23,26 @@ public class NewBehaviourScript : MonoBehaviour
 
     public void OnClick()
     {
-        if (btnF!)
+        if (btnF == false)
         {
             btnF = true;
             Toujou();
+            dark.gameObject.SetActive(true);
+            
+        }else{
+            btnF = false;
+            Taijou();
+            dark.gameObject.SetActive(false);
         }
+
     }
 
     public void Toujou()
     {
-        transform.DOLocalMove(new Vector3(10f, 0, 0), 1f);
+        transform.DOLocalMove(new Vector3(0, 60f, 0), 1f);
+    }
+    public void Taijou()
+    {
+        transform.DOLocalMove(new Vector3(0, 350f, 0), 1f);
     }
 }
