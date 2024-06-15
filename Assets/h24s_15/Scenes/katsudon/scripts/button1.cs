93664862ff06cc489a077e09be33b99fce92d7ca
katsudontech <sumiya.k.ab@m.titech.ac.jp>
2024-06-15 10:38:15 +0900

    ３つの能力セレクト画面仮を作りました

---
+++ Assets/h24s_15/Scenes/katsudon/scripts/button1.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class button1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClick()
+    {
+        Debug.Log("一個目（左のやつ）が押されたよ");  // ログを出力
+    }
+}
---
+++ Assets/h24s_15/Scenes/katsudon/scripts/button3.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class button3 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClick()
+    {
+        Debug.Log("３個目（右のやつ）が押されたよ");  // ログを出力
+    }
+}
