76009ddd1ec5b2f519061c5234fb0c7b730e023d
Astarte <satomi.t.ac@m.titech.ac.jp>
2024-06-15 15:07:09 +0900

    4~6ステージ目も実装

--- Assets/h24s_15/Scenes/Astarte/Program/CanvasSwitcher.cs
+++ Assets/h24s_15/Scenes/Astarte/Program/CanvasSwitcher.cs
@@ -4,6 +4,10 @@ public class CanvasSwitcher : MonoBehaviour
 {
     public GameObject canvas1;
     public GameObject canvas2;
+    public GameObject canvas3;
+    public GameObject canvas4;
+    public GameObject canvas5;
+    public GameObject canvas6;
     public GameManager gameManager;
 
     void Start()
@@ -16,11 +20,47 @@ public class CanvasSwitcher : MonoBehaviour
         if(gameManager.progress == 1){
         canvas1.SetActive(true);
         canvas2.SetActive(false);
+        canvas3.SetActive(false);
+        canvas4.SetActive(false);
+        canvas5.SetActive(false);
+        canvas6.SetActive(false);
         }
         else if(gameManager.progress==2){
         canvas1.SetActive(false);
         canvas2.SetActive(true);
+        canvas3.SetActive(false);
+        canvas4.SetActive(false);
+        canvas5.SetActive(false);
+        canvas6.SetActive(false);
+        }
+        else if(gameManager.progress==3){
+        canvas1.SetActive(false);
+        canvas2.SetActive(false);
+        canvas3.SetActive(true);
+        canvas4.SetActive(false);
+        canvas5.SetActive(false);
+        canvas6.SetActive(false);
+        }else if(gameManager.progress==4){
+        canvas1.SetActive(false);
+        canvas2.SetActive(false);
+        canvas3.SetActive(false);
+        canvas4.SetActive(true);
+        canvas5.SetActive(false);
+        canvas6.SetActive(false);
+        }else if(gameManager.progress==5){
+        canvas1.SetActive(false);
+        canvas2.SetActive(false);
+        canvas3.SetActive(false);
+        canvas4.SetActive(false);
+        canvas5.SetActive(true);
+        canvas6.SetActive(false);
+        }else if(gameManager.progress==6){
+        canvas1.SetActive(false);
+        canvas2.SetActive(false);
+        canvas3.SetActive(false);
+        canvas4.SetActive(false);
+        canvas5.SetActive(false);
+        canvas6.SetActive(true);
         }
     }
 }
-
